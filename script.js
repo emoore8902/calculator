901fb714beccb2c6b20cb59ab226a022215e0777
Elijah Moore <e.moore8902@gmail.com>
2022-06-01 11:45:44 -0700

    added buttons for the calculator and operate function

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(x,y){
 function divide (x,y) {
     return x / y;
 }
+
+function operate (x,y,operator) {
+    switch (operator) {
+        case '+' :
+            add(x,y);
+            break;
+        case '-' :
+            subtract(x,y);
+            break;
+        case '*' :
+            multiply(x,y);
+            break;    
+        case '/' :
+            divide(x,y);
+            break;
+        default:
+            console.log("Not a valid operator");
+    }
+}
